800ee2f939bc0bfc851b280b0ef70f7bde97b87f
Anthony Vicente <anthonyvicente@Anthonys-Air.attlocal.net>
2024-12-21 23:46:34 -0500

    Second commit

---
+++ aboutUs.js
@@ -0,0 +1,5 @@
+let firstDiv = document.querySelector('.intro_div');
+let boxDiv = document.createElement('div');
+boxDiv.classList.add('xo');
+document.body.appendChild(boxDiv);
+
---
+++ sample.js
@@ -0,0 +1,2 @@
+let x = "HELLO WORLD";
+console.log(x);
